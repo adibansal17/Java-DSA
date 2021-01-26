b25009f95619db141087c9dd6fc543570b20eb45
Aditya Bansal <55239429+adibansal17@users.noreply.github.com>
2021-01-27 00:32:57 +0530

    Add files via upload

---
+++ QuickSort.java
@@ -0,0 +1,19 @@
+package Sorting;
+import java.util.*;
+
+public class QuickSort {
+      public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int n,i;
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        for(i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+
+      } 
+      public static int quickSort(arr){
+          
+      }   
+}
